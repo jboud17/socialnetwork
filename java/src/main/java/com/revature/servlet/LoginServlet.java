4a0ace8f57cd5c2442e0a7b9e83a135eca415627
Trevor Fortner <trevorfortner1@gmail.com>
2018-03-21 11:30:32 -0400

    Trying things

--- java/src/main/java/com/revature/servlet/LoginServlet.java
+++ java/src/main/java/com/revature/servlet/LoginServlet.java
@@ -16,7 +16,7 @@ public class LoginServlet extends HttpServlet{
 	 private static final long serialVersionUID = 1L;
 
 	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 response.sendRedirect("Login.html");		//	*******************************
+		 //do nothing, we shouldn't be calling this get
 	 }
 
 	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
