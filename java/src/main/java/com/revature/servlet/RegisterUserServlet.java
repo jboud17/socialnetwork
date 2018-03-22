66a4f0390fd800c0aa3e1be6239df2205e67ce72
Josh Boudreau <jboud1217@gmail.com>
2018-03-22 14:53:41 -0400

    Register user functionality

--- java/src/main/java/com/revature/servlet/RegisterUserServlet.java
+++ java/src/main/java/com/revature/servlet/RegisterUserServlet.java
@@ -3,48 +3,45 @@ package com.revature.servlet;
 import java.io.IOException;
 import java.sql.Blob;
 import java.sql.Timestamp;
+
+import com.revature.beans.User;
 import com.revature.dao.UserDAOClass;
+import com.revature.util.HibernateUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class RegisterUserServlet extends HttpServlet{
 		private static final long serialVersionUID = 1L;
 		
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-
-			String id = req.getParameter("id");
-			int user_id = Integer.parseInt(id);
-
-			String fname = req.getParameter("first_name");
-			String lname = req.getParameter("last_name");
-			String uname = req.getParameter("username");
-			String password = req.getParameter("pswd");
-			String email = req.getParameter("email");
-
-			Timestamp bd = null;
-			
-			UserDAOClass a = new UserDAOClass();
-			
-			a.makeUser(user_id, fname, lname, uname, password, email, bd);
-			
-			try {
-				resp.sendRedirect("http://localhost:4200/home");	//*************************************
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
 
 		/**
 		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 		 *      response)
 		 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response)
-				throws ServletException, IOException {
-			// TODO Auto-generated method stub
-			doGet(request, response);
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			//Set session and transaction objects
+			Session session= HibernateUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			
+			//Get values from register form
+			String fname = req.getParameter("firstName");
+			String lname = req.getParameter("lastName");
+			String uname = req.getParameter("username");
+			String pword = req.getParameter("password");
+			String email = req.getParameter("email");
+			String formDate = req.getParameter("birthday");
+			Timestamp birthdate = null;
+			
+			//Create new user and save in db
+			User user = new User(fname, lname, uname, pword, null, email, birthdate);
+			session.save(user);
 		}
 }
